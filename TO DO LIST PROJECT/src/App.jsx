cc60c3013c30d4bf7a86390356e1a4617a3408c1
DashCodeMaster <165507751+DashCodeMaster@users.noreply.github.com>
2024-10-22 23:02:49 +0530

    Add files via upload

---
+++ TO DO LIST PROJECT/src/App.jsx
@@ -0,0 +1,15 @@
+import TodoList from './TodoList/TodoList'
+import WeatherGet from './WeatherGet/WeatherGet'
+import './App.css'
+
+const App = () => {
+
+  return (
+    <div>
+      <TodoList/>
+{/*       <WeatherGet/> */}
+    </div>
+  )
+}
+
+export default App
